90fda668a9b82bbc2788bc487c745802de190762
Ibukhvalov <159453257+Ibukhvalov@users.noreply.github.com>
2025-01-30 22:31:44 +0300

    removed unnecessary method

--- geometry/point.cpp
+++ geometry/point.cpp
@@ -9,11 +9,6 @@ Point Point::operator+(const Point &other) const {
     return {x+other.x, y+other.y, z+other.z};
 }
 
-Point& Point::with_z(float new_z) {
-    z = new_z;
-    return *this;
-}
-
 
 std::ostream& operator<<(std::ostream& out, const Point& p) {
     std::cout << "( " << p.x << ", " << p.y << ", " << p.z << " )";
